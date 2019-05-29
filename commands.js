ed6c2ca70d4b4bae4bdbae0cbdcc048af3c8792e
S4URABH <saurabhdaware99@gmail.com>
2019-05-29 17:11:44 +0530

    restructuring

---
+++ commands.js
@@ -0,0 +1,100 @@
+const fs = require('fs');
+const vscode = require('vscode');
+
+class TerminalTree{
+	constructor(terminals){
+		this.terminals = terminals;
+		this.i = -1;
+	}
+	getChildren(element){
+		console.log(element);
+		return this.terminals;
+	}
+
+	getTreeItem(element){
+		console.log(element);
+		this.i++;
+		let terminal = this.terminals[this.i];
+		return {
+			label:terminal.label,
+			tooltip:`Open ${terminal.label}`,
+			command:{command:terminal.command,title:terminal.commandTitle}
+		}
+	}
+
+	getParent(element){
+		console.log(element);
+		return 'd';
+	}
+}
+
+
+class TerminalManager{
+    constructor(context){
+        this.context = context;
+    }
+
+    getTerminals(){
+        let terminals;
+        try{
+            terminals = JSON.parse(fs.readFileSync(this.context.globalStoragePath+'/terminals.json'));
+            console.log(terminals);
+            terminals = terminals.map(terminal => {
+                terminal.command = 'extension.'+terminal.label.toLowerCase().replace(/ /g,'');
+                terminal.commandTitle = terminal.label;
+                return terminal;
+            })
+        }catch(e){
+            fs.promises.mkdir(this.context.globalStoragePath,{recursive:true})
+                .then(() => {
+                    return fs.promises.writeFile(this.context.globalStoragePath+'/terminals.json',`[{
+                        "label":"Ubuntu",
+                        "shellPath":"C:\\Windows\\System32\\bash.exe"
+                    },
+                    {
+                        "label":"Windows",
+                        "shellPath":"C:\\Windows\\System32\\cmd.exe"
+                    }]`)
+                })
+                .catch((e) => {
+                    console.log(e);
+                })
+            
+            terminals = [];
+        }
+    
+        return terminals;
+    }
+    
+    loadTerminals(){
+        let terminals = this.getTerminals(this.context);
+        // vscode.workspace.onDidChangeTextDocument()
+        let tree = vscode.window.createTreeView('terminal-manager', {treeDataProvider:  new TerminalTree(terminals)});
+    
+        this.context.subscriptions.push(tree);
+        // vscode.window.showInformationMessage('Congratulations, your extension "terminal-manager" is now active!');
+        for(let terminal of terminals){
+            let cmd = vscode.commands.registerCommand(terminal.command, function () {
+                let terminalInstance = vscode.window.createTerminal(terminal.commandTitle,terminal.shellPath);
+                terminalInstance.show();
+            });
+            this.context.subscriptions.push(cmd);
+        }
+
+        return Promise.resolve(this.context.subscriptions)
+    }
+    
+    loadCommands(){
+        let editTerminals = vscode.commands.registerCommand('extension.editTerminals',() => {
+            let terminalsJsonPath = this.context.globalStoragePath+'/terminals.json';
+            vscode.window.showTextDocument(vscode.Uri.parse("file:///"+terminalsJsonPath))
+                .catch(console.log);
+        })
+        this.context.subscriptions.push(editTerminals);
+
+        return Promise.resolve(this.context.subscriptions);
+    }
+    
+}
+
+module.exports = TerminalManager;
--- extension.js
+++ extension.js
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
 const fs = require('fs');
+const TerminalManager = require('./commands.js')
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
@@ -10,101 +10,25 @@ const fs = require('fs');
  */
 
 // This is supperr hacky way of creating Trees I got no fking clue how to use it in real.
-class TerminalTree{
-	constructor(terminals){
-		this.terminals = terminals;
-		this.i = -1;
-	}
-	getChildren(element){
-		console.log(element);
-		return this.terminals;
-	}
-
-	getTreeItem(element){
-		console.log(element);
-		this.i++;
-		let terminal = this.terminals[this.i];
-		return {
-			label:terminal.label,
-			tooltip:`Open ${terminal.label}`,
-			command:{command:terminal.command,title:terminal.commandTitle}
-		}
-	}
-
-	getParent(element){
-		console.log(element);
-		return 'd';
-	}
-}
-
-function getTerminals(context){
-	let terminals;
-	try{
-		terminals = JSON.parse(fs.readFileSync(context.globalStoragePath+'/terminals.json'));
-		console.log(terminals);
-		terminals = terminals.map(terminal => {
-			terminal.command = 'extension.'+terminal.label.toLowerCase().replace(/ /g,'');
-			terminal.commandTitle = terminal.label;
-			return terminal;
-		})
-	}catch(e){
-		fs.promises.mkdir(context.globalStoragePath,{recursive:true})
-			.then(() => {
-				return fs.promises.writeFile(context.globalStoragePath+'/terminals.json','[]')
-			})
-			.catch((e) => {
-				console.log(e);
-			})
-		
-		terminals = [];
-	}
-
-	return terminals;
-}
-
-function loadTerminals(context){
-	let terminals = getTerminals(context);
-	// vscode.workspace.onDidChangeTextDocument()
-	let tree = vscode.window.createTreeView('terminal-manager', {treeDataProvider:  new TerminalTree(terminals)});
-
-	context.subscriptions.push(tree);
-	// vscode.window.showInformationMessage('Congratulations, your extension "terminal-manager" is now active!');
-	for(let terminal of terminals){
-		let cmd = vscode.commands.registerCommand(terminal.command, function () {
-			let terminalInstance = vscode.window.createTerminal(terminal.commandTitle,terminal.shellPath);
-			terminalInstance.show();
-		});
-		context.subscriptions.push(cmd);
-	}
-}
-
-function loadCommands(context){
-	let helloWorld = vscode.commands.registerCommand('extension.helloWorld',function(){
-		vscode.window.showInformationMessage("Hello Boiii!");
-	})
-	context.subscriptions.push(helloWorld);
-
-	let editTerminals = vscode.commands.registerCommand('extension.editTerminals',function(){
-		let terminalsJsonPath = context.globalStoragePath+'/terminals.json';
-		vscode.window.showTextDocument(vscode.Uri.parse("file:///"+terminalsJsonPath))
-			.catch(console.log);
-	})
-	context.subscriptions.push(editTerminals);
-}
 
 function activate(context) {
-	loadTerminals(context);
-	loadCommands(context);
+	let terminalManager = new TerminalManager(context);
 
-	fs.watchFile(context.globalStoragePath+'//terminals.json',function(event){
-		console.log(event);
-		for(let disposable of context.subscriptions){
-			disposable.dispose();
-		}
-		// tree.dispose();
-		loadTerminals(context);
-		loadCommands(context);
-	})
+	terminalManager.loadTerminals()
+		.then(()=>{
+			return terminalManager.loadCommands();
+		})
+		.then((subscriptions) => {
+			fs.watchFile(context.globalStoragePath+'//terminals.json',function(event){
+				console.log(event);
+				for(let disposable of subscriptions){
+					disposable.dispose();
+				}
+				// tree.dispose();
+				terminalManager.loadTerminals();
+				terminalManager.loadCommands();
+			})
+		})
 }
 exports.activate = activate;
 
